6e760245e7a3401326eafec9e708e200c4fcd5ba
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-11-14 11:32:54 -0500

    chore(C++): renovate build, data, project

--- cpp/build.rs
+++ cpp/build.rs
@@ -3,6 +3,8 @@ use cxx_build;
 fn main() {
     cxx_build::bridge("src/main.rs") // returns a cc::Build
         .file("src/main.cpp")
+        .file("src/lib/employee.cpp")
+        .file("src/lib/utils.cpp")
         .flag_if_supported("-std=c++11")
         .compile("lab-template");
 
